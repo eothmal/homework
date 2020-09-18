2b94cb506dba00aae4ec8d09ced13e03b9ace6b0
eothmal@att.net <eothmal@att.net>
2020-09-18 15:47:11 -0500

    update

---
+++ demo/src/main/java/com/example/enrollment/controller/CourseEnrollmentControllerDb.java
@@ -0,0 +1,74 @@
+package com.example.enrollment.controller;
+
+import com.example.enrollment.domain.Course;
+import com.example.enrollment.domain.Student;
+import com.example.enrollment.domain.StudentsCoursesMapping;
+import com.example.enrollment.handler.EnrollmentServiceDb;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class CourseEnrollmentControllerDb {
+
+@Autowired private EnrollmentServiceDb enrollmentService;
+
+    @GetMapping("/students")
+    public List<Student> getStudents() {
+        return enrollmentService.getStudents();
+    }
+
+    @GetMapping("/courses")
+    public List<Course> getCourses() {
+        return enrollmentService.getCourses();
+    }
+
+    @GetMapping("/students/{studentId}")
+        public Student getStudentDetails(@PathVariable String studentId) {
+        return  enrollmentService.retrieveStudent(studentId);
+    }
+
+    @GetMapping("/courses/{courseId}")
+    public Course getCourseDetails(@PathVariable String courseId) {
+        return enrollmentService.retrieveCourse(courseId);
+    }
+
+    @PostMapping("/students")
+    public String createStudent(@RequestBody Student student) {
+        enrollmentService.addStudent(student);
+        return "success";
+    }
+
+    @PostMapping("/courses")
+    public String createCourse(@RequestBody Course course) {
+        enrollmentService.addCourse(course);
+        return "success";
+    }
+
+    @GetMapping("/students/{studentId}/courses")
+    public List<Course> retrieveCoursesForAStudent(@PathVariable String studentId)
+    {
+        return enrollmentService.retrieveCoursesForAStudent(studentId);
+    }
+
+    @GetMapping("/courses/{courseId}/students")
+    public List<Student> retrieveStudentsForACourse(@PathVariable String courseId)
+    {
+        return enrollmentService.retrieveStudentsForACourse(courseId);
+    }
+    @PostMapping("/enroll")
+    public String enroll(@RequestBody Map<String, String> json) {
+        String id = json.get("id");
+        String studentId = json.get("studentId");
+        String courseId = json.get("courseId");
+        boolean ret = enrollmentService.enrollInACourse(id, studentId, courseId);
+        return ret ? "Success": "Failure";
+    }
+    @GetMapping("/enroll")
+    public Iterable<StudentsCoursesMapping> getStudentsCoursesMapping() {
+        return enrollmentService.getStudentsCoursesMappings();
+    }
+
+}
---
+++ demo/src/main/java/com/example/enrollment/handler/EnrollmentServiceDb.java
@@ -0,0 +1,86 @@
+package com.example.enrollment.handler;
+
+import com.example.enrollment.dao.CourseRepository;
+import com.example.enrollment.dao.StudentRepository;
+import com.example.enrollment.dao.StudentsCoursesRepository;
+import com.example.enrollment.domain.Course;
+import com.example.enrollment.domain.Student;
+import com.example.enrollment.domain.StudentsCoursesMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+@Component
+public class EnrollmentServiceDb {
+
+    @Autowired
+    private CourseRepository courseRepository;
+    @Autowired
+    private StudentRepository studentRepository;
+    @Autowired
+    private StudentsCoursesRepository studentsCoursesRepository;
+
+    public Student retrieveStudent(String studentId) {
+        return studentRepository.findById(studentId).get();
+    }
+
+    public Course retrieveCourse(String courseId) {
+        return courseRepository.findById(courseId).get();
+    }
+
+    public boolean enrollInACourse(String id,String studentId, String courseId) {
+        StudentsCoursesMapping studentsCoursesMapping =
+                new StudentsCoursesMapping(Integer.valueOf(id), studentId, courseId);
+        StudentsCoursesMapping ret = studentsCoursesRepository.save(studentsCoursesMapping);
+        return ret != null ? true : false;
+    }
+
+    public List<Course> retrieveCoursesForAStudent(String studentId) {
+        List<StudentsCoursesMapping> studentsCoursesMappings = studentsCoursesRepository.findByStudentId(studentId);
+
+        List<Course> courses = new ArrayList<>();
+        for (StudentsCoursesMapping courseMapping : studentsCoursesMappings) {
+            Course course = courseRepository.findById(courseMapping.getCourseId()).get();
+            courses.add(course);
+        }
+        return courses;
+    }
+
+    public List<Student> retrieveStudentsForACourse(String courseId) {
+        List<StudentsCoursesMapping> studentsCoursesMappings = studentsCoursesRepository.findByCourseId(courseId);
+
+        List<Student> students = new ArrayList<>();
+        for (StudentsCoursesMapping studentMapping : studentsCoursesMappings) {
+            Student student = studentRepository.findById(studentMapping.getStudentId()).get();
+            students.add(student);
+        }
+        return students;
+    }
+
+    public List<Course> getCourses() {
+        return StreamSupport.stream(courseRepository.findAll().spliterator(), false)
+                .collect(Collectors.toList());
+    }
+
+
+    public List<Student> getStudents() {
+        return StreamSupport.stream(studentRepository.findAll().spliterator(), false)
+                .collect(Collectors.toList());
+    }
+
+    public void addCourse(Course course) {
+        courseRepository.save(course);;
+    }
+
+    public void addStudent(Student student) {
+        studentRepository.save(student);;
+    }
+
+    public Iterable<StudentsCoursesMapping> getStudentsCoursesMappings() {
+        return studentsCoursesRepository.findAll();
+    }
+}
