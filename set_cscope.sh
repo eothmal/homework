ce2ecd5c971c303d085c62ef64d7c2d26b4734a0
eothmal@att.net <eothmal@att.net>
2021-03-17 16:18:22 -0500

    update

--- set_cscope.sh
+++ set_cscope.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# find . -name '*.java' > cscope.files
+# find . -name '*.html' >> cscope.files
 mkdir -p ~/.vim/plugin && \
 cp /workspace/homework/*.vim ~/.vim/plugin/. && \
 cd /workspace/homework && \
