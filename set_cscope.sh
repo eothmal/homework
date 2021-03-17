19fe6ecc9e0567c1cd2c71d75c393c3ba9c8b4e7
eothmal <eothmal@att.net>
2021-03-17 19:40:11 +0000

    add vimrc

--- set_cscope.sh
+++ set_cscope.sh
@@ -3,5 +3,5 @@ mkdir -p ~/.vim/plugin && \
 cp /workspace/homework/*.vim ~/.vim/plugin/. && \
 cd /workspace/homework && \
 cscope -b
-cp /workspace/homework/dotvirc ~/.vimrc
+cp /workspace/homework/dotvimrc ~/.vimrc
 
